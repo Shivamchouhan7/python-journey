bcf997c2814cc310c8e2f4137c69a8a12ac5fe15
shivam <shivamchouhan123sc@gmail.com>
2026-01-08 21:51:09 +0530

    Determine Data type of variables using type() function

---
+++ Variable_and_Datatypes/type.py
@@ -0,0 +1,35 @@
+# checking the data type of variable using type
+
+
+a = 10                # Integer
+b = 12.5              # Float
+c = "Hello Python"    # String
+d = True              # Boolean
+e = [1, 2, 3]         # List
+f = (4, 5, 6)         # Tuple
+g = {1, 2, 3}         # Set
+h = {"name": "Ram"}   # Dictionary
+
+# Printing values and their data types
+
+print("Value:", a, "Type:", type(a))
+print("Value:", b, "Type:", type(b))
+print("Value:", c, "Type:", type(c))
+print("Value:", d, "Type:", type(d))
+print("Value:", e, "Type:", type(e))
+print("Value:", f, "Type:", type(f))
+print("Value:", g, "Type:", type(g))
+print("Value:", h, "Type:", type(h))
+
+
+# type casting 
+# Type Casting Example
+
+x = "10"          # string
+y = int(x)        # string → integer
+
+l = 5             # integer
+m = float(l)      # integer → float
+
+print("Value:", y, "Type:", type(y))
+print("Value:", m, "Type:", type(m))
