67eb6d7cab1667cf83690de5e73c2510a27781fa
shivam <shivamchouhan123sc@gmail.com>
2026-01-08 19:22:07 +0530

    Variables and Data types

---
+++ Variable_and_Datatypes/Variable.py
@@ -0,0 +1,9 @@
+a = 1  # a is variable containing integer datatype
+
+b = 20.5   #b is floating point number 
+
+c = "name"  #c is string
+
+d = False #d is boolean variable 
+
+e = None # e is None type variable 
---
+++ Variable_and_Datatypes/Variable_name.py
@@ -0,0 +1,36 @@
+# 1. Variable name must start with a letter (a–z / A–Z) or an underscore (_)
+# 2. Variable name cannot start with a number
+# 3. Variable name can contain letters, numbers, and underscore only
+# 4. No spaces are allowed in variable names
+# 5. Variable names are case-sensitive
+# 6. Python keywords cannot be used as variable names
+# Valid variable names
+
+name = "Shivam"
+_age = 18
+marks2 = 85
+total_marks = 450
+StudentName = "Amit"
+
+print("Name:", name)
+print("Age:", _age)
+print("Marks:", marks2)
+print("Total Marks:", total_marks)
+print("Student Name:", StudentName)
+
+
+# Case-sensitive example
+marks = 80
+Marks = 90
+
+print("\nCase Sensitive Example:")
+print("marks =", marks)
+print("Marks =", Marks)
+
+
+# Invalid variable names 
+
+# 1name = "Ram"          # Cannot start with number
+# total-marks = 500      # Special character not allowed (-)
+# total marks = 500      # Space not allowed 
+# class = 10             # Keyword cannot be used (class is a keyword)
